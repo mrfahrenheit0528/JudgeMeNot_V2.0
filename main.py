72e0db67cafb592ee287a762fc305f3120765fb8
mrfahrenheit0528 <guhermoso@my.cspc.edu.ph>
2026-03-09 01:10:14 +0800

    feat: Implement initial web application structure with dashboard UI and reorganize project files into dedicated directories.

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Hello World")
+import os
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, 
+                instance_relative_config=True,
+                template_folder='webapp/templates',
+                static_folder='webapp/static')
+    
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # A simple page that says hello
+    @app.route('/')
+    def index():
+        return render_template('index.html', title="Home")
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5000)
--- webapp/python/database.py
+++ webapp/python/database.py
@@ -10,7 +10,7 @@ db_name = "judgemenot_v2_db"
 
 # Currently resolving to SQLite for ease of testing during development
 # In production, replace the engine below with standard mysql strings
-DATABASE_URL = "sqlite:///judgemenot_v2.db"
+DATABASE_URL = "sqlite:///instance/judgemenot_v2.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
